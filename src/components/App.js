de5e96f1ae9cecbc9852fab352e27ec833dc9d88
sara toftegaard <satoftegaard@gmail.com>
2016-12-20 11:40:54 -0500

    basic layout

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,30 @@ import React, { Component } from 'react'
 class App extends Component {
 
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
+    return <div className='app'>
+      <h1>Explosion Avoider</h1>
+      <div className='gameboard'>
+        <table>
+          <tbody>
+            <tr>
+              <td className='empty' />
+              <td className='numbered'>3</td>
+              <td className='flagged' />
+            </tr>
+            <tr>
+              <td className='bomb' />
+              <td className='unrevealed' />
+              <td className='flagbomb' />
+            </tr>
+            <tr>
+              <td className='unrevealed' />
+              <td className='unrevealed' />
+              <td className='unrevealed' />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <footer>Potatoes made with love at the Iron Yard.</footer>
     </div>
   }
 }
