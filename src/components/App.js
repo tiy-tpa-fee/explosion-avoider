f1a9cd0ab328820119d2cf566e4bb1cc0f4afa1f
Stephan Joseph <stephanjoseph2007@gmail.com>
2016-12-20 11:40:50 -0500

    Grid Created

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,31 @@ import React, { Component } from 'react'
 class App extends Component {
 
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
+    return <div className='app'>
+      <h1>Explosion Avoider!</h1>
+      <div className='gameboard'>
+        <table>
+          <tbody>
+            <tr>
+              <td className='empty' />
+              <td className='numberd'>3</td>
+              <td className='flagged' />
+            </tr>
+
+            <tr>
+              <td className='bombed' />
+              <td className='unrevealed' />
+              <td className='flagbomb' />
+            </tr>
+            <tr>
+              <td className='unrevealed' />
+              <td className='unrevealed' />
+              <td className='unrevealed' />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <footer> Potatos made with love at the Iron Yard.</footer>
     </div>
   }
 }
